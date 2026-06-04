9ba8749022b2e4f4a064fb36b287488ca5448055
Ishika Agrawal <ishikaagrawal2132007@gmail.com>
2026-06-04 17:50:56 +0530

    Implemented basic input and output concepts in C++
    * Added a beginner-level C++ program demonstrating input and output operations.
    * Used `cin` for user input handling and `cout` for displaying output.
    * Reinforced understanding of variables, data flow, and console interaction.
    * Organized code with proper formatting and readability practices.

--- 01_basics/input_output.cpp
+++ 01_basics/input_output.cpp
@@ -1,13 +1,14 @@
+//Program: First Github repository setup
+// Concepts: Basic Output
+
 #include <iostream>
 
 // Using explicit scoping instead of 'using namespace std;' is preferred in production 
+
 using std::cout;
 using std::endl;
 
 int main(){
-    //Fast I/O for performance optimize
-    std::ios_base::sync_with_studio(false);
-    std::cin.tie(NULL);
     
     cout<<"Hello, Github! Repository initialized successfully."<<endl;
     
