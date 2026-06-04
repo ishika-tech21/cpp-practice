18174a08e25c3d7a5f237934767ecaf7f6685e8b
Ishika Agrawal <ishikaagrawal2132007@gmail.com>
2026-06-04 15:06:18 +0530

    Create students_grades_tracker.cpp

---
+++ 01_basics/students_grades_tracker.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+int main() {
+    cout << "=== Student Grade Average Calculator " << endl;
+
+    int totalSubjects = 0;
+    cout << "Enter the number of subjects: ";
+    cin >> totalSubjects;
+    //Guard rail:check for valid input
+    if (totalSubjects <= 0) {
+       cout << "Invalid number of subjects." << endl;
+       return 0;
+    }
+    double sum = 0.0;
+    double marks = 0.0;
+
+    //Using a simple loop to calculate total marks 
+    for (int i = 1; i <= totalSubjects; i++) {
+        cout << "Enter marks for subject: "<< i << ":";
+        cin >> marks;
+        sum = sum + marks;
+    }
+   //Calculate average 
+   double average = sum / totalSubjects;
+
+   cout << "\n--- Results ---" << endl;
+   cout << "Total Marks: " << sum << endl;
+   cout << "Average Score: " << average << "%" << endl;
+
+   return 0;
+}
+
+
+
